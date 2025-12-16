8f9f32a213126810aa82df856a261af1b7efa832
Pragya <pendharkarp23@gmail.com>
2025-12-17 03:57:52 +0530

    created section 3

---
+++  03-counter-state/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1>Let's build our first project in react</h1>
+      <p>im using react,javascript and bun in this project</p>
+    </>
+  );
+}
