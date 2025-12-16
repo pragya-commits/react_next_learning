8821269579a7edb349c072d148da395045f4ebad
Pragya <pendharkarp23@gmail.com>
2025-12-16 12:50:42 +0530

    first commit

---
+++ 02-design-work/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-600 dark:text-sky-400 border-2 p-4">
+        It's a Design work project
+      </h1>
+
+      <p>Using react + tailwindcss</p>
+    </>
+  );
+}
+
+export default App;
