2df2002f8279b2534b6a5e342c3415da9ccd4b19
Pragya <pendharkarp23@gmail.com>
2025-12-16 16:20:01 +0530

    Added some images in card

--- 02-design-work/src/App.jsx
+++ 02-design-work/src/App.jsx
@@ -8,16 +8,19 @@ function App() {
       </h1>
       <div className="flex gap-4">
         <Card
+          imageUrl="https://static.vecteezy.com/system/resources/previews/027/127/463/non_2x/javascript-logo-javascript-icon-transparent-free-png.png"
           title="Join now javascript course"
           buttonText="Pay Now"
           description="This is a javascript course , we are using tailwind in this course as a css.there are many projects in this course which you can add in your portfolio. "
         />
         <Card
+          imageUrl="https://prod-discovery.edx-cdn.org/cdn-cgi/image/width=auto,height=auto,quality=75,format=webp/media/course/image/381a0046-5d78-4790-8776-74620d59f48e-e2e7f4677ce2.jpeg"
           title="Join now python course"
           buttonText="Pay Now"
           description="This is a python course , we are using tailwind in this course as a css.there are many projects in this course which you can add in your portfolio. "
         />
         <Card
+          imageUrl="https://www.tisdigitech.com/wp-content/uploads/2024/06/React.png"
           title="Join now react course"
           description="This is a react course , we are using tailwind in this course as a css.there are many projects in this course which you can add in your portfolio. "
           buttonText="Pay Now"
--- 02-design-work/src/components/Card.jsx
+++ 02-design-work/src/components/Card.jsx
@@ -2,16 +2,17 @@ function Card({
   title = "default",
   buttonText = "default",
   description = "default",
+  imageUrl = "default",
 }) {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-xl mt-8 shadow overflow-hidden transition-shadow">
       <img
-        className="w-full h-70 object-cover"
-        src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
+        className="w-full h-70 object-cover p-5"
         alt="sample image"
+        src={imageUrl}
       />
       <div className="p-4">
-        <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
+        <h2 className="text-lg font-semibold text-gray-800 ">{title}</h2>
         <p className="mt-2 text-gray-600 text-sm">{description}</p>
         <button className="mt-4 px-4 bg-blue-800 p-2 rounded-lg text-white py-2 hover:bg-blue-600">
           {buttonText}
