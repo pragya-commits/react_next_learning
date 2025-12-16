13b61682f426a436b2865b5551412f0d7c60bc38
Pragya <pendharkarp23@gmail.com>
2025-12-16 16:02:20 +0530

    Added Card component

--- 02-design-work/src/App.jsx
+++ 02-design-work/src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
-
+import Card from "./components/Card.jsx";
 function App() {
   return (
     <>
       <h1 className="text-blue-600 dark:text-sky-400 border-2 p-4">
         It's a Design work project
       </h1>
-
-      <p>Using react + tailwindcss</p>
+      <div className="flex gap-4">
+        <Card
+          title="Join now javascript course"
+          buttonText="Pay Now"
+          description="This is a javascript course , we are using tailwind in this course as a css.there are many projects in this course which you can add in your portfolio. "
+        />
+        <Card
+          title="Join now python course"
+          buttonText="Pay Now"
+          description="This is a python course , we are using tailwind in this course as a css.there are many projects in this course which you can add in your portfolio. "
+        />
+        <Card
+          title="Join now react course"
+          description="This is a react course , we are using tailwind in this course as a css.there are many projects in this course which you can add in your portfolio. "
+          buttonText="Pay Now"
+        />
+      </div>
     </>
   );
 }
---
+++ 02-design-work/src/components/Card.jsx
@@ -0,0 +1,23 @@
+function Card({
+  title = "default",
+  buttonText = "default",
+  description = "default",
+}) {
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-xl mt-8 shadow overflow-hidden transition-shadow">
+      <img
+        className="w-full h-70 object-cover"
+        src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
+        alt="sample image"
+      />
+      <div className="p-4">
+        <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
+        <p className="mt-2 text-gray-600 text-sm">{description}</p>
+        <button className="mt-4 px-4 bg-blue-800 p-2 rounded-lg text-white py-2 hover:bg-blue-600">
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Card;
