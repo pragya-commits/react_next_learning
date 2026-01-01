fe104393ed5a0db0df84103e7efa836b7ae74258
Pragya <pendharkarp23@gmail.com>
2026-01-01 13:36:25 +0530

    did some changes in file

--- 04-queue-management/src/App.jsx
+++ 04-queue-management/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+export default function App() {
+  const [queue, setQueue] = useState([]);
 
-function App() {
-  const [count, setCount] = useState(0)
+  const addToQueue = (customer) => {
+    //add data to queue
+  };
+
+  const updateStatus = (id, newStatus) => {
+    //change data in queue
+  };
+
+  const removeFromQueue = (id) => {
+    //change data from the queue
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header>
+        <h1>Queue Management Application</h1>
+        <p>Manage yoour customer's efficiently</p>
+      </header>
+      <main>
+        <h1>Queue Form</h1>
+        <h1>Queue Display</h1>
+      </main>
+    </div>
+  );
 }
-
-export default App
