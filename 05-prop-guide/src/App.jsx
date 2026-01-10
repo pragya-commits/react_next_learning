19e4e171d1ef20e3a21e1178fb1c6eb29e9ccc0c
Pragya <pendharkarp23@gmail.com>
2026-01-10 12:11:38 +0530

    initial setup is completed

--- 05-prop-guide/src/App.jsx
+++ 05-prop-guide/src/App.jsx
@@ -1,11 +1,35 @@
 import "./App.css";
 
-function App() {
+import BasicProps from "./components/BasicProps.jsx";
+import ChildrenProps from "./components/ChildrenProps.jsx";
+import ComplexProps from "./components/ComplexProps.jsx";
+import RefProps from "./components/RefProps.jsx";
+import ThemeProps from "./components/ThemeToggler.jsx";
+
+function Navigation() {
+  const isDark = true;
+
+  const sections = [
+    { id: "basic", label: "Basic Props", icon: "📦" },
+    { id: "ref", label: "ref Props", icon: "🔗" },
+    { id: "children", label: "children Props", icon: "👶🏻" },
+    { id: "complex", label: "complex Props", icon: "🧩" },
+    { id: "theme", label: "theme Props", icon: "🎨" },
+  ];
+
+  return <nav className={`sticky top-0 z-50 shadow-md`}></nav>;
+}
+
+function AppContent() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className={`min-h-screen bg-gray-800 text-white`}>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   );
+}
+
+function App() {
+  return <AppContent />;
 }
 
 export default App;
